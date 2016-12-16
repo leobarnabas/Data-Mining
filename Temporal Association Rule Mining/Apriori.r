bb5fe42710d6309299a2df8cfbd8d4f607d4d4e6
leobarnabas <leobarnabas@gmail.com>
2016-12-16 16:20:21 -0500

    Add files via upload

--- Temporal Association Rule Mining/Apriori.r
+++ Temporal Association Rule Mining/Apriori.r
@@ -5,6 +5,17 @@ script.dir <- dirname(sys.frame(1)$ofile);
 setwd(script.dir);
 txn = read.transactions(file="nodefailure_cleaned.txt", format="basket",sep=",");
 
-rules=apriori(txn, parameter = list(target="rules",support=0.7, confidence=0.7, minlen=2, maxlen=4));
+# plotting the frequencies
+itemFrequencyPlot(txn, topN = 20)
+
+# apriori computation
+rules=apriori(txn, parameter = list(target="rules",support=0.5, confidence=0.7, minlen=2, maxlen=6));
 inspect(rules);
 plot(rules);
+
+#Plot graph-based visualisation:
+
+subrules2 <- head(sort(rules, by="lift"), 10)
+
+plot(subrules2, method="graph",control=list(type="items",main=""))
+
